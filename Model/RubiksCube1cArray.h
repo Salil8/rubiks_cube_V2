e53c0266d81db34424f445a6b2ddca955262c2e3
Salil8 <salillokhande14@gmail.com>
2024-06-26 10:42:38 +0530

    update

---
+++ Model/RubiksCube1cArray.h
@@ -0,0 +1,8 @@
+//
+// Created by salil on 25-06-2024.
+//
+
+#ifndef RUBIKSCUBE1CARRAY_H
+#define RUBIKSCUBE1CARRAY_H
+
+#endif //RUBIKSCUBE1CARRAY_H
---
+++ PatternDatabases/CornerDBMaker.h
@@ -0,0 +1,26 @@
+//
+// Created by salil on 25-06-2024.
+//
+
+#ifndef CORNERDBMAKER_H
+#define CORNERDBMAKER_H
+
+#include "CornerPatternDatabase.h"
+#include "../Model/RubiksCubeBitboard.cpp"
+
+using namespace std;
+
+class CornerDBMaker {
+private:
+    string fileName;
+    CornerPatternDatabase cornerDB;
+
+public:
+    CornerDBMaker(string _fileName);
+    CornerDBMaker(string _fileName, uint8_t init_val);
+
+    bool bfsAndStore();
+};
+
+
+#endif //CORNERDBMAKER_H
